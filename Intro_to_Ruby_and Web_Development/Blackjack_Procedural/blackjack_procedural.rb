9c8f5f6a6341fea53c93b1af007eb55bf98fb773
Laurence Kauffman <lgkauffman@yahoo.com>
2013-07-04 08:09:33 -0700

    put in betting, app not working

--- Intro_to_Ruby_and Web_Development/Blackjack_Procedural/blackjack_procedural.rb
+++ Intro_to_Ruby_and Web_Development/Blackjack_Procedural/blackjack_procedural.rb
@@ -4,9 +4,10 @@
 
 def calculate_hand (hand)
 	total = 0
+	hand2 = hand.dup # duplicate array so this method doesn't mutate/empty the original
 
-	while hand.size != 0
-		card = hand.pop
+	while hand2.size != 0
+		card = hand2.pop
 
 		case card[1]
 		when 'A'
@@ -31,17 +32,25 @@ def calculate_hand (hand)
 end
 
 
-def list_cards_and_total(cards_to_list)
-	puts "Your cards: " + cards_to_list.to_s
-	total = calculate_hand(cards_to_list)
-	puts "Your total is: " + total.to_s
+def list_cards(person, cards_to_list)
+	puts person + " cards are: " + cards_to_list.to_s
 end
-
+	
 
 def wager
-	puts 'You have $' + player_money.to_s
-	puts 'How much do you want to wager for this hand?'
-	bet = gets.chomp.to_i
+	while true
+		puts 'You have $' + player_money.to_s
+		puts 'How much do you want to wager for this hand?'
+		bet = gets.chomp.to_i
+		if bet > player_money
+			puts "Bet exceeds your funds.  Please try again."
+		else
+			player_money = player_money - bet
+			puts "You have bet $" + bet.to_s
+			bet
+	end
+
+
 end
 
 
@@ -54,11 +63,10 @@ full_deck =
 
 
 player_money = 500
-
+bet = 0
 puts "Enter Your Name: "
 player_name = gets.chomp
 puts 'Hello, ' + player_name + ', Welcome to Blackjack!'
-
 
 # puts bet.to_s
 play_again = 'y'
@@ -67,27 +75,69 @@ while true
   # deck = fullDeck.shuffle(random: Random.new(1))   
 	#### =>>	test in irb and with pry to see what's happening - gives same order every time
 	deck = full_deck.shuffle
-	player_cards = [deck.pop, deck.pop]
-	dealer_cards = [deck.pop, deck.pop]
-	puts list_cards_and_total(player_cards)
 	
+	dealer_cards = [deck.pop, deck.pop]
+	player_cards = [deck.pop, deck.pop]
+		# OR player_cards = []; player_cards << deck.pop; player_cards << deck.pop
+  	# OR player_cards = []; player_cards << deck.pop << deck.pop
+	dealer_total = 0
+	player_total = 0
+
+	# bet = wager
+	puts list_cards("Your ", player_cards)
+	puts "Your total is: " + calculate_hand(player_cards).to_s
+
+
 	while true
+		bust = 'n'
 		puts "Do you want a hit? (y/n)"
 		hit = gets.chomp
 		
 		if hit == 'y'
 			player_cards << deck.pop
-			puts list_cards_and_total(player_cards)
+			puts list_cards("Your ", player_cards)
+			puts "Your total is: " + calculate_hand(player_cards).to_s
+			player_total = calculate_hand(player_cards)
+
+			if player_total > 21
+				bust = 'y'
+				break
+			end
 		else
 			break
 		end
 	end
 
-	puts list_cards_and_total(player_cards)
+
+	if bust == 'y'
+		puts "You Bust"
+	else
+		dealer_total =  calculate_hand(dealer_cards)
+		
+		while dealer_total < 17
+			dealer_cards << deck.pop
+			dealer_total =  calculate_hand(dealer_cards)
+		end
+
+		puts list_cards("Dealer ", dealer_cards)
+		puts "Dealer Total is: " + dealer_total.to_s
+
+		if dealer_total > 21 || dealer_total < player_total 
+			player_money += bet
+			puts "YOU WIN!  You now have $" + player_money
+		elsif dealer_total == player_total
+			puts "It's a Draw.    You now have $" + player_money
+		else
+			player_money -= bet
+			puts "You lose.  You now have $" + player_money
+		end 
+	end
+
 	puts 'Play Again? (y/n)'
 	play_again = gets.chomp
-	
+
 	if play_again == 'n'
 		break
 	end
+end
 end
