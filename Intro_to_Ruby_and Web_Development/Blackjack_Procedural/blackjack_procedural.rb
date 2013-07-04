204db274c08351c69a705873f554e2606eadbb99
Laurence Kauffman <lgkauffman@yahoo.com>
2013-07-03 20:00:56 -0700

    in progress

--- Intro_to_Ruby_and Web_Development/Blackjack_Procedural/blackjack_procedural.rb
+++ Intro_to_Ruby_and Web_Development/Blackjack_Procedural/blackjack_procedural.rb
@@ -5,30 +5,45 @@
 def calculate_hand (hand)
 	total = 0
 
-	while hand.pop != nil
+	while hand.size != 0
 		card = hand.pop
 
-		case card
-		when card[1] == 'A'
-		if total + 11 > 21
-		  total += 1
+		case card[1]
+		when 'A'
+			if total + 11 > 21
+		  	total += 1
 		  else
-		  total += 11
+		  	total += 11
 		  end
-
-		when card[1] == 'J'
-		total += 10
-		when card[1] == 'Q'
-		total += 10
-		when card[1] == 'K'
-		total += 10
+		when '1'
+			total += 10
+		when 'J'
+			total += 10
+		when 'Q'
+			total += 10
+		when 'K'
+			total += 10
 		else
-		total += card[1].to_i
+			total += card[1].to_i
 		end
-puts "total in loop: " + total.to_s
 	end
 	total
 end
+
+
+def list_cards_and_total(cards_to_list)
+	puts "Your cards: " + cards_to_list.to_s
+	total = calculate_hand(cards_to_list)
+	puts "Your total is: " + total.to_s
+end
+
+
+def wager
+	puts 'You have $' + player_money.to_s
+	puts 'How much do you want to wager for this hand?'
+	bet = gets.chomp.to_i
+end
+
 
 full_deck = 
 [ 'HA', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HQ', 'HK',
@@ -36,51 +51,39 @@ full_deck =
 	'CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'D10', 'CJ', 'CQ', 'CK',
 	'SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ', 'SK' ]
 
+
+
 player_money = 500
 
 puts "Enter Your Name: "
 player_name = gets.chomp
 puts 'Hello, ' + player_name + ', Welcome to Blackjack!'
 
-puts 'You have $' + player_money.to_s
-puts 'How much do you want to wager for this hand?'
-bet = gets.chomp.to_i
 
 # puts bet.to_s
 play_again = 'y'
 
 while true
-	puts 'in loop'
-
-	# deck = fullDeck.shuffle(random: Random.new(1))   
-	# 	test in irb and with pry to see what's happening - gives same order every time
-	
+  # deck = fullDeck.shuffle(random: Random.new(1))   
+	#### =>>	test in irb and with pry to see what's happening - gives same order every time
 	deck = full_deck.shuffle
-	# puts deck
-
 	player_cards = [deck.pop, deck.pop]
 	dealer_cards = [deck.pop, deck.pop]
+	puts list_cards_and_total(player_cards)
 	
-	puts "Your cards: " + player_cards.to_s
-	total = calculate_hand(player_cards)
-	puts "Your total is: " + total.to_s
-
 	while true
 		puts "Do you want a hit? (y/n)"
 		hit = gets.chomp
 		
 		if hit == 'y'
 			player_cards << deck.pop
+			puts list_cards_and_total(player_cards)
 		else
 			break
 		end
-
-		puts "Your cards: " + player_cards.to_s
 	end
 
-	puts "Your cards: " + player_cards.to_s
-
-	
+	puts list_cards_and_total(player_cards)
 	puts 'Play Again? (y/n)'
 	play_again = gets.chomp
 	
@@ -88,4 +91,3 @@ while true
 		break
 	end
 end
-	
