86ac0535a3f57c5ecd8b3ce1f31eab30476bd592
Laurence Kauffman <lgkauffman@yahoo.com>
2013-07-03 18:16:36 -0700

    in progress

--- Intro_to_Ruby_and Web_Development/Blackjack_Procedural/blackjack_procedural.rb
+++ Intro_to_Ruby_and Web_Development/Blackjack_Procedural/blackjack_procedural.rb
@@ -2,44 +2,106 @@
 # by Laurence Kauffman
 # July 1, 2013
 
-fullDeck = ['HA', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'HJ', 'HQ', 'HK',
-				'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'DJ', 'DQ', 'DK',
-				'CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'CJ', 'CQ', 'CK',
-				'AA', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'AJ', 'AQ', 'AK']
+def calculate_hand (hand)
+	total = 0
 
-playerMoney = 500
+	while hand.pop != nil
+		card = hand.pop
+
+		case card
+		when card.at(1) == 'A'
+			if total + 11 > 21
+		  	total += 1
+		  else
+		  	total += 11
+		  end
+
+		when card.at(1) == "2"
+			total += 2
+		when card.at(1) == '3'
+			total += 3
+		when card.at(1) == '4'
+			total += 4
+		when card.at(1) == '5'
+			total += 5
+		when card.at(1) == '6'
+			total += 6
+		when card.at(1) == '7'
+			total += 7
+		when card.at(1) == '8'
+			total += 8
+		when card.at(1) == '9'
+			total += 9
+		when card.at(1) == '1'
+			total += 10
+		when card.at(1) == 'J'
+			total += 10
+		when card.at(1) == 'Q'
+			total += 10
+		when card.at(1) == 'K'
+			total += 10
+		# else
+		end
+	end
+	total
+end
+
+full_deck = 
+[ 'HA', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HQ', 'HK',
+	'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ', 'DK',
+	'CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'D10', 'CJ', 'CQ', 'CK',
+	'SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ', 'SK' ]
+
+player_money = 500
 
 puts "Enter Your Name: "
-playerName = gets.chomp
-puts 'Hello, ' + playerName + ', Welcome to Blackjack!'
+player_name = gets.chomp
+puts 'Hello, ' + player_name + ', Welcome to Blackjack!'
 
-puts 'You have $' + playerMoney.to_s
+puts 'You have $' + player_money.to_s
 puts 'How much do you want to wager for this hand?'
 bet = gets.chomp.to_i
 
 # puts bet.to_s
-playAgain = 'y'
+play_again = 'y'
 
 while true
 	puts 'in loop'
 
-	# deck = [] 
-	deck = [ fullDeck.shuffle(random: Random.new(1)) ]  
-	  # puts deck
-
-	playerCards = [ ]
-	playerCards << deck.pop
-	playerCards << deck.pop
+	# deck = fullDeck.shuffle(random: Random.new(1))   
+	# 	test in irb and with pry to see what's happening - gives same order every time
 	
-	puts playerCards
-	# playerCard2 = deckInPlay.pop
-	# dealerCards = []
+	deck = full_deck.shuffle
+	# puts deck
 
-	puts 'Play Again? (y/n)'
-	playAgain = gets.chomp
+	player_cards = [deck.pop, deck.pop]
+	dealer_cards = [deck.pop, deck.pop]
 	
-	if playAgain == 'n'
-		break
+	puts "Your cards: " + player_cards.to_s
+	total = calculate_hand(player_cards)
+	puts "Your total is: " + total
+	
+	while true
+		puts "Do you want a hit? (y/n)"
+		hit = gets.chomp
+		
+		if hit == 'y'
+			player_cards << deck.pop
+		else
+			break
+		end
+
+		puts "Your cards: " + player_cards.to_s
 	end
 
+	puts "Your cards: " + player_cards.to_s
+
+	
+	puts 'Play Again? (y/n)'
+	play_again = gets.chomp
+	
+	if play_again == 'n'
+		break
+	end
 end
+	
