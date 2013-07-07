3a75b51df069cc0d2d0c0e928aad8e0b2af368be
Laurence Kauffman <lgkauffman@yahoo.com>
2013-07-07 13:49:01 -0700

    updated

--- Intro_to_Ruby_and Web_Development/Blackjack_Procedural/blackjack_procedural.rb
+++ Intro_to_Ruby_and Web_Development/Blackjack_Procedural/blackjack_procedural.rb
@@ -4,15 +4,17 @@
 
 require 'pry'
 
+dealer_total = 0
+player_total = 0
+play_again = 'y'
+hit = 'y'
+bust = 'n'
+
 full_deck = 
 [ 'HA', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HQ', 'HK',
   'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ', 'DK',
   'CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'D10', 'CJ', 'CQ', 'CK',
   'SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ', 'SK' ]
-
-dealer_total = 0
-player_total = 0
-play_again = 'y'
 
 def calculate_hand (hand) 
   total = 0
@@ -43,71 +45,68 @@ def calculate_hand (hand)
   total
 end
 
+
 print "Enter Your Name: "
 player_name = gets.chomp
 puts "Hello, " + player_name + ", Welcome to Blackjack!"
 puts
 
-while true
+# Play game
+while play_again == 'y'
   deck = full_deck.shuffle
-  
   dealer_cards = [deck.pop, deck.pop]
   player_cards = [deck.pop, deck.pop]
 
-  puts "Dealer's Up Card is: " + dealer_cards[0]
+  # define method display_hands here to avoid runtime error:
+  # `display_hands': undefined local variable or method `dealer_cards'
+  def display_hands
+  puts "Dealer's Up Card is: " + dealer_cards[0].to_s
   puts "Your Cards are:    " + player_cards.to_s  
   puts "Your Total is: " + calculate_hand(player_cards).to_s
+  end
 
-  while true
-    bust = 'n'
+  # Ask if player wants a hit, check for bust
+  while hit = 'y' && bust = 'n'
+    display_hands
     print "Do you want a hit? (y/n) "
     hit = gets.chomp
     puts
 
     if hit == 'y'
       player_cards << deck.pop
-      puts "Dealer's Up Card is: " + dealer_cards[0]
-      puts "Your Cards are:    " + player_cards.to_s  
-      puts "Your Total is: " + calculate_hand(player_cards).to_s
-      player_total = calculate_hand(player_cards)
+    end
 
-      if player_total > 21
-        bust = 'y'
-        break
-      end
-    else
-      break
+    if player_total > 21
+      bust = 'y'
     end
   end
 
+  # If you bust then end game, else continue
   if bust == 'y'
-    puts "You Bust"
-    break
+    puts "You Bust."
   else
-    dealer_total =  calculate_hand(dealer_cards)
-    
-    while dealer_total < 17
+    # Dealer gets cards until s/he hits hard 17
+    while dealer_total < 17 # less than hard 17
       dealer_cards << deck.pop
-      dealer_total =  calculate_hand(dealer_cards)
+      dealer_total =  calculate_hand(dealer_cards) 
     end
 
+    # List Dealer cards and total
     puts "Dealer Cards are: " + dealer_cards.to_s
     puts "Dealer Total is: " + dealer_total.to_s
 
-    if dealer_total > 21 || dealer_total < player_total 
+    # Calculate outcome of game
+    if dealer_total > 21 
+      puts "Dealer busts.  YOU WIN!"
+    elsif dealer_total < player_total 
       puts "YOU WIN!"  
     elsif dealer_total == player_total
       puts "It's a Push."    
-    else
+    elsif dealer_total > player_total
       puts "You lose."
     end 
-    bet = 0
   end
 
   puts 'Play Again? (y/n)'
   play_again = gets.chomp
-
-  if play_again == 'n'
-    break
-  end
-end
+end    
